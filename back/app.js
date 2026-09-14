db901bc68ee58baa813cf5df2d1a190d2d7660a8
Shinemi <simon.lucas.83136@gmail.com>
2026-09-14 11:46:49 +0200

    feat: update server configuration and add database connection setup

--- back/app.js
+++ back/app.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
 
-// url
-app.get('/', (req,res) => {
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
     res.send('bienvenue sur mon API RESTful !')
 })
 
-app.listen(port,() => {
-    //ce console log s'affiche uniquement côté serveur et non côté client
-    console.log(`Serveur lancé sur https://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`)
 })
