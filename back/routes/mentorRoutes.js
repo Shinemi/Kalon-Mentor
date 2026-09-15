f8dadefa5c4f0c23a316099849ea012af3a9dd56
Shinemi <simon.lucas.83136@gmail.com>
2026-09-15 16:52:46 +0200

    feat: add mentorController, mentorRoutes, and corresponding test file

---
+++ back/routes/mentorRoutes.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const router = express.Router()
+const { } = require('../controllers/mentorController')
+const authMiddleware = require('../middlewares/authMiddleware')
+
+
+router.post('/upload', )
+router.post('/correction', )
+router.post('/recommend', )
+
+module.exports = router
